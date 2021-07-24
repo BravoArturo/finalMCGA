72a5487a95d18d7e4c117f4dfaba89af42c23ad2
BravoArturo <aarturoo97@gmail.com>
2021-07-24 18:58:33 -0300

    add and test functions to get, create, delete and update tasks

--- src/routes/Tasks.controller.ts
+++ src/routes/Tasks.controller.ts
@@ -1,11 +1,22 @@
+import { error } from 'console';
 import {RequestHandler} from 'express';
 import Task from './Task'
-export const getTasks : RequestHandler = (req, res) => {
-
-    res.json('getting tasks');
+export const getTasks : RequestHandler = async (req, res) => {
+    try{
+        const tasks = await Task.find();
+        return res.json(tasks);
+    }catch(error){
+        res.json(error)
+    }
 };
-export const getTask : RequestHandler = (req, res) => {
-    res.json('getting a task');
+export const getTask : RequestHandler = async (req, res) => {
+    try{
+        const taskFound = await Task.findById(req.params.id);
+        if (!taskFound) return res.status(204).json();
+        res.json(taskFound);
+    }catch(error){
+        res.json(error)
+    }
 };
 export const createTask : RequestHandler = async(req, res) => {
     const task = new Task(req.body);
@@ -13,9 +24,21 @@ export const createTask : RequestHandler = async(req, res) => {
     console.log(savedTask);    
     res.json('creating a new tasks');
 };
-export const deleteTasks : RequestHandler = (req, res) => {
-    res.json('deleting an existing task');
+export const deleteTasks : RequestHandler = async (req, res) => {
+    try{
+        const taskDeleted = await Task.findByIdAndDelete(req.params.id);
+        if (!taskDeleted) return res.status(204).json();
+        return res.json('One task was deleted ' + taskDeleted);
+    }catch(error){
+        res.json(error)
+    }
 };
-export const updateTasks : RequestHandler = (req, res) => {
-    res.json('updating an existing task');
+export const updateTasks : RequestHandler = async (req, res) => {
+    try{
+        const taskUpdated = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if (!taskUpdated) return res.status(204).json();
+        return res.json('One task was Updated ' + taskUpdated);
+    }catch(error){
+        res.json(error)
+    }
 };
