d418d86f155b58c5428c8b62a697586184c0f604
BravoArturo <aarturoo97@gmail.com>
2021-07-24 16:22:50 -0300

    testing post a new task

--- src/routes/Task.routes.ts
+++ src/routes/Task.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { getTasks } from "./Tasks.controller";
+import * as taskController from "./Tasks.controller";
 const router = Router(); //This allow to me to use new routes.
 
-router.get('/Tasks', getTasks);
-
+router.get('/Tasks', taskController.getTasks);
+router.get('/Tasks/:id', taskController.getTask);
+router.post('/Tasks', taskController.createTask);
+router.put('/Tasks/:id', taskController.updateTasks);
+router.delete('/Tasks/:id', taskController.deleteTasks);
 export default router;
--- src/routes/Tasks.controller.ts
+++ src/routes/Tasks.controller.ts
@@ -1,5 +1,20 @@
 import {RequestHandler} from 'express';
-
+import Task from './Task'
 export const getTasks : RequestHandler = (req, res) => {
+
+    res.json('getting tasks');
+};
+export const getTask : RequestHandler = (req, res) => {
+    res.json('getting a task');
+};
+export const createTask : RequestHandler = (req, res) => {
+    const task = new Task(req.body);
+    console.log(task);    
+    res.json('getting tasks');
+};
+export const deleteTasks : RequestHandler = (req, res) => {
+    res.json('getting tasks');
+};
+export const updateTasks : RequestHandler = (req, res) => {
     res.json('getting tasks');
 };
