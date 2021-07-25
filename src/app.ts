a4aecc6c1d0bc7851ec03d106f5335fd5548a517
BravoArturo <aarturoo97@gmail.com>
2021-07-25 11:40:47 -0300

    change the number of port

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import taskRoutes from './routes/Task.routes';
 
 const app = express();
 
-app.set('port', 3000);
+app.set('port', 4000);
 app.use(express.json());
 app.use(express.urlencoded({extended: false})); //When is sent some information by a post from a form it can understand what contains.
 app.use(morgan('dev')); //this give me information about the proccess.
