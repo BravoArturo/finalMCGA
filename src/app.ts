4719c136f77468702f050fe46c5c8ccc4a640ab4
BravoArturo <aarturoo97@gmail.com>
2021-07-25 12:59:41 -0300

    add cors to allow to connect

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
 import config from './config';
+import cors from 'cors';
 import taskRoutes from './routes/Task.routes';
 
 const app = express();
 
-app.set('port', 4000);
+app.set('port', 3000);
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false})); //When is sent some information by a post from a form it can understand what contains.
 app.use(morgan('dev')); //this give me information about the proccess.
 app.use(taskRoutes);
