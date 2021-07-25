a36c30acf8069533ddf8251b3789b59f6f079bb4
BravoArturo <aarturoo97@gmail.com>
2021-07-25 13:01:05 -0300

    get information from my api

--- src/components/screens/Task/TaskList.tsx
+++ src/components/screens/Task/TaskList.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 const TaskList = () => {
+    
+    const loadTask = async () => {
+        const res = await axios.get('http://localhost:3000/Tasks');
+        console.log(res);
+    }
+    useEffect(() => {
+        loadTask()
+    }, [])
     return (
         <div>Hola soy la lista</div>
-    )
+    );
 }
 export default TaskList;
