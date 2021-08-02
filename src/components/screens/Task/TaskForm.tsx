e9be78f3ce3d0f9a9c6dbe48d9ad009710a64116
Garello Lautaro <lautarogarello99@gmail.com>
2021-08-01 23:48:05 -0300

    Create/Update task design

--- src/components/screens/Task/TaskForm.tsx
+++ src/components/screens/Task/TaskForm.tsx
@@ -11,22 +11,22 @@ const TaskForm = () => {
     }
     const params = useParams<Params>();
     console.log(params);
-    const initalState = {title: '', description: '', deadline: ''};
+    const initalState = { title: '', description: '', deadline: '' };
     const [task, settask] = useState<Task>(initalState);
 
     const hadleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        settask({...task, [e.target.name]: e.target.value})
+        settask({ ...task, [e.target.name]: e.target.value })
     };
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault(); //we cancelled the default behavior
 
-        if(!params.id){
+        if (!params.id) {
             const res = await TaskService.createTask(task);
             toast.success('New Task added');
             console.log(res.data);
             settask(initalState);
-        }else{
+        } else {
             await TaskService.updatedTask(params.id, task);
             toast.success('Task updated');
         }
@@ -35,28 +35,46 @@ const TaskForm = () => {
 
     const getTask = async (id: string) => {
         const res = await TaskService.getTask(id);
-        const {title, description, deadline} = res.data;
-        settask({title,description,deadline});
+        const { title, description, deadline } = res.data;
+        settask({ title, description, deadline });
     }
-    useEffect(()=> {
+    useEffect(() => {
         if (params.id) getTask(params.id);
     }, [])
     return (
         <div>
             New Task
-            <form onSubmit={handleSubmit}>
-                <div>
-                <input type="text" name="title" placeholder="Write a title for this task" autoFocus onChange={hadleInputChange} value={task.title}></input>
-                <textarea name="description" placeholder="Please enter a description" onChange={hadleInputChange} value={task.description}></textarea>
-                <input type="date" name="deadline" placeholder="Please enter a deadline" onChange={hadleInputChange} value={task.deadline}></input>
+            <div className="wrapper">
+                <div className='newTask'>
+                    <form onSubmit={handleSubmit}>
+                        <div className="taskStructure">
+                            <div className="deadline">
+                                <label>Please enter a deadline</label>
+                                <input type="date" name="deadline" onChange={hadleInputChange} value={task.deadline}></input>
+                            </div>
+                            <div className="tittle">
+                                <label>Write a title for this task</label>
+                                <input type="text" name="title" autoFocus onChange={hadleInputChange} value={task.title}></input>
+                            </div>
+                            <div className="description">
+                                <label>Please enter a description</label>
+                                <textarea name="description" onChange={hadleInputChange} value={task.description}></textarea>
+                            </div>
+                            <div className="btnTask">
+                                {
+                                    params.id ?
+                                        <button>Updated task</button>
+                                        :
+                                        <button>Create task</button>
+                                }
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                {
-                    params.id?
-                    <button>Updated task</button>
-                    :
-                    <button>Create task</button>
-                }
-            </form>
+                <div className="footer">
+                    <p>Home</p>
+                </div>
+            </div>
         </div>
     )
 }
